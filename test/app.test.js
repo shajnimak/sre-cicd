6ef81f57a453bb32ba995caed0b9bc5e5a68bbe6
shajnimak <79740295+shajnimak@users.noreply.github.com>
2024-06-08 19:45:38 +0500

    init

---
+++ test/app.test.js
@@ -0,0 +1,25 @@
+// test/app.test.js
+const request = require('supertest');
+const { startServer, stopServer } = require('../app');
+
+
+let server;
+
+
+beforeAll(() => {
+  server = startServer();
+});
+
+
+afterAll(() => {
+  stopServer();
+});
+
+
+describe('GET /', () => {
+  it('should return Hello World!', async () => {
+    const res = await request(server).get('/');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello World!');
+  });
+});
